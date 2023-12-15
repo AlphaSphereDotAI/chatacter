cad9695615451ad5c1e7d294bc13b3676c2ff273
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2023-12-15 19:54:43 +0200

    "Add Overview.md to the Graduation Project documentation"
    This commit includes the addition of the "Overview.md" file into the Graduation Project. This newly added file provides comprehensive information about the dataset used in the project, its attributes and a relevant academic paper. It also adds an overview section in the table of contents in the 'main.md' file. This will improve the structure and readability of the project documentation.

--- chatacter/chatacter/settings.py
+++ chatacter/chatacter/settings.py
@@ -71,12 +71,12 @@ WSGI_APPLICATION = "chatacter.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.yml.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.yml.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
