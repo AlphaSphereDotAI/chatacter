8db7b051df60b9c0ec39db1bf1a038163297fa37
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2023-12-15 20:35:03 +0200

    h

--- Backend/chatacter/urls.py
+++ Backend/chatacter/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from chatacter.views import index
+from Backend.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- Backend/chatacter/views.py
+++ Backend/chatacter/views.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-
 from django.http import HttpResponse
 
 
