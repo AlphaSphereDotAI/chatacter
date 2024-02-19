56bc7cde4b2df168022e932ef1ac589565ef6274
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2024-02-19 18:12:20 +0200

    feat: Add fleet plugins and configure auto save
    Added fleet plugins "fleet.ai", "fleet.git.frontend", "fleet.image.view", "fleet.intellij", "fleet.python", and "fleet.smartMode" to the fleet settings. Also configured auto save to occur after a delay of 5 minutes.
    
    The SadTalker.py file was modified to remove redundant import statements and commented out code related to installing packages and updating dependencies.
    
    A new import statement for the "os" module was added.
    
    The code now includes a commented command for activating a virtual environment.
    
    The code now displays GPU information and the installed Python version.
    
    The code now changes the current directory to "sadtalker".
    
    The code now downloads pre-trained models if they do not already exist.
    
    The code now includes a commented command for installing the required packages.
    
    The code now runs the inference script with specific image and audio sources, checkpoint directory, and results directory.
    
    A new file, "AI/SadTalker/wget-log", was added.

--- AI/SadTalker.py
+++ AI/SadTalker.py
@@ -1,23 +1,24 @@
-import matplotlib.pyplot as plt
-import os, glob, gradio
-from SadTalker.inference import main
+import os
 
-# apt-get update
-# apt install software-properties-common -y
-# dpkg --remove --force-remove-reinstreq python3-pip python3-setuptools python3-wheel
-# apt-get install python3-pip -y
-# apt autoremove -y
-# git clone https://github.com/Winfredy/SadTalker
-# pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
-# apt update
-# apt install ffmpeg -y
+'''
+apt-get update
+apt install software-properties-common -y
+dpkg --remove --force-remove-reinstreq python3-pip python3-setuptools python3-wheel
+apt-get install python3-pip -y
+apt autoremove -y
+git clone https://github.com/Winfredy/SadTalker
+pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 \
+--extra-index-url https://download.pytorch.org/whl/cu113
+apt update
+apt install ffmpeg -y
+'''
 
 # Now, the virtual environment is created and you can install packages using pip.
 # os.system("source activate chatacter")
 os.system("nvidia-smi --query-gpu=name,memory.total,memory.free --format=csv,noheader")
 os.system("python --version")
-os.chdir("SadTalker")
-os.system("pip install -r requirements.txt")
+os.chdir("sadtalker")
+# os.system("pip install -r requirements.txt")
 
 print("Download pre-trained models...")
 if not os.path.exists("checkpoints"):
@@ -27,25 +28,17 @@ if not os.path.exists("checkpoints"):
 else:
     print("Pre-trained models already exist.")
 
+image = "examples/source_image/art_0.png"
+audio = "examples/driven_audio/bus_chinese.wav"
+results = "results"
+os.system(f"python inference.py \
+            --source_image {image} \
+            --driven_audio {audio} \
+            --checkpoint_dir checkpoints \
+            --result_dir {results}")
 
-print("Choose the image name to animate: (saved in folder 'examples/')")
-img_list = glob.glob1("examples/source_image", "*.png")
-img_list.sort()
-img_list = [item.split(".")[0] for item in img_list]
-default_head_name = "art_0"
-
-# selected audio from exmaple/driven_audio
-img = "examples/source_image/{}.png".format(default_head_name)
-print(img)
-
-gradio.inputs.Image(type="pil", label="Source Image")
-gradio.outputs.Video(label="Animated Video")
-gradio.Interface(
-    fn=main(
-        img,
-        "examples/driven_audio/bus_chinese.wav",
-        "examples/source_image/{}.png".format(default_head_name),
-    ),
-    inputs="image",
-    outputs="video",
-).launch()
+# gradio.Interface(
+#     fn=,
+#     inputs='textbox',
+#     outputs='video',
+# ).launch()
