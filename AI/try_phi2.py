4b6c7ca029d5145238814bd6bb77e1043bb7c74a
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2024-02-20 19:33:11 +0200

    Delete unnecessary files and update gitignore

--- AI/try_phi2.py
+++ AI/try_phi2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 import time
@@ -17,23 +16,3 @@ outputs = model.generate(inputs, min_length=1000, max_length=1000, num_beams=4, 
 
 et = time.time()
 print(et - st)
-=======
-from transformers import AutoTokenizer, AutoModelForCausalLM
-import torch
-import time
-
-torch.set_default_device("cuda")
-model = AutoModelForCausalLM.from_pretrained(
-    "microsoft/phi-2", torch_dtype="auto", trust_remote_code=True
-)
-tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
-
-st = time.time()
-inputs = tokenizer.encode("how are you?", return_tensors="pt")
-# outputs = model.generate(inputs, max_length=32, num_beams=4, early_stopping=True)
-# print(tokenizer.decode(outputs[0]))
-outputs = model.generate(inputs, min_length=1000, max_length=1000, num_beams=4, early_stopping=True)
-
-et = time.time()
-print(et - st)
->>>>>>> f5424afa1ac4cd3c51a45df37e24eeff80606872
