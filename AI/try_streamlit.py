b602eb2f57b0358bd66eadc7eb6432fe881945ba
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2024-03-06 23:40:37 +0200

    Update try_streamlit.py

--- AI/try_streamlit.py
+++ AI/try_streamlit.py
@@ -4,7 +4,7 @@ import os
 import google.generativeai as genai
 import pandas as pd
 
-genai.configure(api_key="")
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 
 # import pprint
 # for model in genai.list_models():
