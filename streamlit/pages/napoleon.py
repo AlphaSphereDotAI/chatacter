9a81b84a80c0c367f5cb5299e892f184cf7a90e4
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2024-04-17 20:36:23 +0200

    Refactor chat_input variable name in napoleon.py

--- streamlit/pages/napoleon.py
+++ streamlit/pages/napoleon.py
@@ -1,8 +1,5 @@
 import json
-
 import requests
-from torch import t
-
 import streamlit as st
 
 API = "localhost:8000"  # "https://8000-01hqrk1qr2p3w6cc5np0wk0ys5.cloudspaces.litng.ai"
@@ -29,12 +26,11 @@ def request_prediction(query: str):
 
 
 st.title("😎 Chat with Napoleon Bonaparte")
-query = st.chat_input("Type your message here")
+userInput = st.chat_input("Type your message here")
 
-if query is not None:
+if userInput is not None:
     message = st.chat_message("human")
-    # write number to indecate the time taken in seconds
     message.write("🕒")
-    response, audio = request_prediction(query)
+    response, audio = request_prediction(userInput)
     message.write(response)
     st.audio(audio, format="audio/wav")
