aca1a5f6bec20a47ce097418d030e4858da430ca
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2024-04-20 20:51:48 +0000

    Update configuration files for backend and bark-small

--- backend/model.py
+++ backend/model.py
@@ -1,18 +1,26 @@
-import json
 import os
 
+import pandas as pd
 from huggingface_hub import snapshot_download
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
 from scipy.io.wavfile import write
 from transformers import AutoModelForTextToWaveform, AutoProcessor
 
-CONFIG = json.load(open("/workspaces/graduation_project/config.json"))
-snapshot_download(
-    repo_id="suno/bark-small", local_dir=CONFIG["text_to_voice_model_dir"]
-)
-processor = AutoProcessor.from_pretrained(CONFIG["text_to_voice_model_dir"])
-model = AutoModelForTextToWaveform.from_pretrained(CONFIG["text_to_voice_model_dir"])
+CONFIG = pd.read_json("/workspaces/graduation_project/config.json")
+
+
+def check_and_download_model(model_path, repo_id):
+    if not os.path.exists(model_path):
+        print("Model not found. Downloading...")
+        snapshot_download(repo_id=repo_id, local_dir=model_path)
+    else:
+        print("Model exists.")
+
+
+check_and_download_model(CONFIG["model"]["text_to_voice"], "suno/bark-small")
+processor = AutoProcessor.from_pretrained(CONFIG["model"]["text_to_voice"])
+model = AutoModelForTextToWaveform.from_pretrained(CONFIG["model"]["text_to_voice"])
 chat = ChatGroq(model_name="mixtral-8x7b-32768", verbose=True)
 
 
@@ -23,14 +31,14 @@ def generate_audio(response):
     audio = model.generate(**inputs)
     print("\tAudio generated with Rate 24000")
     print("\tSaving audio...")
-    write(CONFIG["audio"], 24000, audio.squeeze(0).numpy())
+    write(CONFIG["assets"]["audio"], 24000, audio.squeeze(0).numpy())
 
 
 def generate_video():
     """generate video"""
-    os.system(
-        f"python /workspaces/graduation_project/backend/sadtalker/inference.py --source_image {CONFIG['image']} --driven_audio {CONFIG['audio']}"
-    )
+    # os.system(
+    #     f"python /workspaces/graduation_project/backend/sadtalker/inference.py --source_image {CONFIG['image']} --driven_audio {CONFIG['audio']}"
+    # )
 
 
 def get_response(query):
